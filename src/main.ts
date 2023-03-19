baa44111b66860ee9a79c69bfe145883cefcf449
Frederic Brodbeck <der.froedi@gmail.com>
2023-03-19 16:38:42 +0100

    settings / shortcut

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,55 @@
 import '@logseq/libs';
+import type { SettingSchemaDesc, SimpleCommandKeybinding } from '@logseq/libs/dist/LSPlugin';
 
 
 const main = async () => {
 	console.log('here');
-	logseq.App.registerCommandPalette(
-		// 'test',
+	if (!logseq) {
+		console.error('`logseq` object not available');
+		return;
+	}
+
+	const settings: SettingSchemaDesc[] = [
 		{
-			key: 'xxxTest',
-			label: 'XXX test',
-			// desc: 'test description',
-			// palette?: booleanString,
-			// keybinding?: SimpleCommandKeybindingString,
+			key: "goToSymbol",
+			title: 'Go to symbol...',
+			description: "Jump to a heading within the current page",
+			default: "mod+t",
+			type: "string",
+		},
+	];
+	logseq.useSettingsSchema(settings);
+
+	const keyBinding: SimpleCommandKeybinding = {
+		// mode: 'editing', // 'global' | 'non-editing'
+		binding: logseq.settings!.goToSymbol,
+		// mac?: string;
+	};
+
+	// logseq.App.registerCommand(
+	// 	"goToSymbol",
+	// 	{
+	// 		key: "goToSymbol",
+	// 		label: 'Go to symbol...',
+	// 		keybinding: keyBinding,
+	// 	},
+	// 	(err) => {
+	// 		console.error(err);
+	// 	}
+	// );
+
+	logseq.App.registerCommandPalette(
+		{
+			key: 'goToSymbol',
+			label: 'Go to symbol...',
+			keybinding: keyBinding,
 		},
 		async () => {
 			const page = await logseq.Editor.getCurrentPage();
-			const blocks = await logseq.Editor.getPageBlocksTree(page?.name);
+			if (!page) {
+				return;
+			}
+			const blocks = await logseq.Editor.getPageBlocksTree(page.name);
 			console.log(blocks);
 		}
 	)
