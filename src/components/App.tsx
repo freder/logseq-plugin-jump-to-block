fbe26f183d32719e44d22852a563a19a49b4ac57
Frederic Brodbeck <der.froedi@gmail.com>
2023-03-25 13:43:56 +0100

    activated theme colors again

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import CommandPalette, { Command } from 'react-command-palette';
 import markdownToTxt from 'markdown-to-txt';
 import * as R from 'ramda';
+import { Global, css } from '@emotion/react';
 
 // @ts-ignore
 import theme from '../../node_modules/react-command-palette/dist/themes/sublime-theme';
@@ -163,22 +164,64 @@ function App() {
 		[]
 	);
 
-	return <CommandPalette
-		open={open}
-		closeOnSelect
-		alwaysRenderCommands
-		highlightFirstSuggestion
-		resetInputOnOpen
-		placeholder="Type to filter..."
-		hotKeys={[]}
-		trigger={null}
-		theme={theme}
-		commands={items}
-		maxDisplayed={500} // hard max. limit
-		onHighlight={(item) => selectionHandler(item, false)}
-		onSelect={(item) => selectionHandler(item, true)}
-		onRequestClose={closeHandler}
-	/>;
+	const computedStyles = getComputedStyle(
+		window.parent.document.documentElement
+	);
+	const bg = computedStyles.getPropertyValue(
+		'--ls-secondary-background-color'
+	);
+	const bgSelection = computedStyles.getPropertyValue(
+		'--ls-a-chosen-bg'
+	);
+	const text = computedStyles.getPropertyValue(
+		'--ls-primary-text-color'
+	);
+	const textSelection = computedStyles.getPropertyValue(
+		'--ls-secondary-text-color'
+	);
+	const input = computedStyles.getPropertyValue(
+		'--ls-primary-background-color'
+	);
+
+	return <Fragment>
+		<Global styles={css`
+			.sublime-modal, .sublime-suggestionsList .sublime-suggestion {
+				background: ${bg} !important;
+				color: ${text} !important;
+			}
+			.sublime-suggestionsList .sublime-suggestionHighlighted {
+				background: ${bgSelection} !important;
+				color: ${textSelection} !important;
+			}
+			.sublime-suggestion {
+				background: ${bgSelection} !important;
+			}
+			.sublime-input {
+				background: ${input} !important;
+				color: ${text} !important;
+			}
+			*::-webkit-scrollbar-thumb {
+				background-color: ${bgSelection} !important;
+				border: solid 3px ${bg} !important;
+			}
+		`} />
+		<CommandPalette
+			open={open}
+			closeOnSelect
+			alwaysRenderCommands
+			highlightFirstSuggestion
+			resetInputOnOpen
+			placeholder="Type to filter…"
+			hotKeys={[]}
+			trigger={null}
+			theme={theme}
+			commands={items}
+			maxDisplayed={500} // hard max. limit
+			onHighlight={(item) => selectionHandler(item, false)}
+			onSelect={(item) => selectionHandler(item, true)}
+			onRequestClose={closeHandler}
+		/>
+	</Fragment>;
 }
 
 export default App;
