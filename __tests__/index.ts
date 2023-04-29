7fa10e5f511d740083b13102629c29982fa50ad3
Frederic Brodbeck <der.froedi@gmail.com>
2023-04-30 00:45:29 +0200

    added tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,9 +1,24 @@
-import { prepareLabel } from '../src/utils/index';
+import { cleanLabel } from '../src/utils/index';
 
 
-describe('prepareLabel', () => {
+describe('cleanLabel', () => {
 	it('should remove properties', () => {
-		const result = prepareLabel('11');
-		expect(result).toEqual(undefined);
+		const content = `## asdf
+id:: 643135d6-33b2-4aea-a78c-e6b3e0ce0f80
+test-asdf::  12 12`;
+		const result = cleanLabel(content);
+		expect(result).toEqual('## asdf');
+	});
+
+	it('should remove todo items', () => {
+		const content = 'TODO buy milk';
+		const result = cleanLabel(content);
+		expect(result).toEqual('buy milk');
+	});
+
+	it('should image dimensions', () => {
+		const content = '![](./asdf.png) {:width 400}';
+		const result = cleanLabel(content);
+		expect(result).toEqual('![](./asdf.png)');
 	});
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,18 @@
 import markdownToTxt from 'markdown-to-txt';
 
 
-export const prepareLabel = (blockContent: string) => {
-	return markdownToTxt(blockContent)
+export const cleanLabel = (s: string) => {
+	return s
 		// collapsed:: true
 		// id:: 643135d6-33b2-4aea-a78c-e6b3e0ce0f80
-		.replaceAll(/^[\w_-]+::\W+([\w_-])+/gmi, '')
+		.replaceAll(/^[\w_-]+::\W+.*$/gmi, '')
 		// {:width 400}
 		.replaceAll(/\{:.*\}/gi, '')
 		.replaceAll(/^(TODO|DOING|DONE|NOW|LATER) /g, '')
 		.trim();
+}
+
+
+export const prepareLabel = (blockContent: string) => {
+	return cleanLabel(markdownToTxt(blockContent));
 };
