9be3f4c04df434bb7bf131a03fa2633a97b5d19b
Frederic Brodbeck <der.froedi@gmail.com>
2023-04-30 00:14:17 +0200

    fixed / improved regexes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,10 +3,11 @@ import markdownToTxt from 'markdown-to-txt';
 
 export const prepareLabel = (blockContent: string) => {
 	return markdownToTxt(blockContent)
-		// ::collapsed true
-		.replaceAll(/[^\W\n]+::\W[^\W]+/gmi, '')
+		// collapsed:: true
+		// id:: 643135d6-33b2-4aea-a78c-e6b3e0ce0f80
+		.replaceAll(/^[\w_-]+::\W+([\w_-])+/gmi, '')
 		// {:width 400}
-		.replaceAll(/\{:.*\}/gmi, '')
-		.replaceAll(/^(TODO|DOING|DONE) /gmi, '')
+		.replaceAll(/\{:.*\}/gi, '')
+		.replaceAll(/^(TODO|DOING|DONE|NOW|LATER) /g, '')
 		.trim();
 };
