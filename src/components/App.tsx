28b5c6e19db2209bf47ab8756ca22dce6d211fa9
Frederic Brodbeck <der.froedi@gmail.com>
2023-04-30 13:30:00 +0200

    moved function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,9 +5,10 @@ import type { InitalSelectionOption, ModeOption } from '../types';
 import React, { useEffect, useState, Fragment } from 'react';
 import CommandPalette, { Command } from 'react-command-palette';
 import * as R from 'ramda';
-import { Global, css } from '@emotion/react';
+import { Global } from '@emotion/react';
 
 import { prepareLabel } from '../utils';
+import { makeStyles } from '../utils/theme';
 import {
 	defaultMaxDepth,
 	headingRegex,
@@ -135,52 +136,6 @@ const makeCommands = (
 };
 
 
-const makeStyles = () => {
-	const computedStyles = getComputedStyle(
-		window.parent.document.documentElement
-	);
-	const bg = computedStyles.getPropertyValue(
-		'--ls-secondary-background-color'
-	);
-	const bgSelection = computedStyles.getPropertyValue(
-		'--ls-a-chosen-bg'
-	);
-	const text = computedStyles.getPropertyValue(
-		'--ls-primary-text-color'
-	);
-	const textSelection = computedStyles.getPropertyValue(
-		'--ls-secondary-text-color'
-	);
-	const input = computedStyles.getPropertyValue(
-		'--ls-primary-background-color'
-	);
-	return css`
-		.sublime-modal, .sublime-suggestionsList .sublime-suggestion {
-			background: ${bg} !important;
-			color: ${text} !important;
-		}
-		.sublime-suggestionsList .sublime-suggestionHighlighted {
-			background: ${bgSelection} !important;
-			color: ${textSelection} !important;
-		}
-		.sublime-suggestion {
-			background: ${bgSelection} !important;
-		}
-		.sublime-input {
-			background: ${input} !important;
-			color: ${text} !important;
-		}
-		*::-webkit-scrollbar-thumb {
-			background-color: ${bgSelection} !important;
-			border: solid 3px ${bg} !important;
-		}
-		.indentation {
-			color: ${bgSelection} !important;
-		}
-	`;
-};
-
-
 function App() {
 	const mode: ModeOption = logseq.settings?.mode || modeDefault;
 
@@ -245,7 +200,7 @@ function App() {
 	const defaultInputValue = '';
 
 	return <Fragment>
-		<Global styles={makeStyles} />
+		<Global styles={makeStyles()} />
 		<CommandPalette
 			open={open}
 			closeOnSelect
